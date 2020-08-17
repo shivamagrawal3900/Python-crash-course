d51ffcb1edd851031d672c50765dfbabd310263b
Shivam Agrawal <shivam.agrawal.3900@gmail.com>
2020-08-17 10:50:34 +0530

    looping through dictionay

--- chapter6-dictionaries.py
+++ chapter6-dictionaries.py
@@ -71,3 +71,164 @@ print(alien_0.get('points', 'No points value assigned'))
 
 ## LOOPING THROUGH A DICTIONARY
 
+
+user_0 = {
+	'username': 'sAgrawal',
+	'first': 'Shivam',
+	'last': 'Agrawal',
+}
+
+# the method items() returns a list of key-value pairs.
+for key, value in user_0.items():
+	print(f"\nKey: {key}")
+	print(f"Value: {value}")
+
+# >Key: username
+# >Value: sAgrawal
+
+# >Key: first
+# >Value: Shivam
+
+# >Key: last
+# >Value: Agrawal
+
+print() #line gap
+# Looping Through All the Keys in a Dictionary
+# use method keys() to return list of keys
+for key in user_0.keys():
+	print(f"key: {key}")
+
+# >key: username
+# >key: first
+# >key: last
+
+print() #line gap
+# Looping through the keys is actually the default behavior when looping through a dictionary, 
+# so this code would have exactly the same output
+for key in user_0:
+	print(f"key: {key}")
+
+# >key: username
+# >key: first
+# >key: last
+
+print() #line gap
+# You can also use the keys() method to find out if a particular key is present
+if 'middle' not in user_0.keys():
+	print(f"middle name not present for user: {user_0['username']}\n")
+
+# >middle name not present for user: sAgrawal
+
+
+# Looping Through a Dictionary’s Keys in a Particular Order
+# Starting in Python 3.7, looping through a dictionary returns the items in the same order they were inserted. 
+# Sometimes, though, you’ll want to loop through a dictionary in a different order.
+favourite_language = {
+	'jen': 'java',
+	'saraha': 'scala',
+	'phil': 'swift',
+	}
+
+for name in sorted(favourite_language.keys()):
+	print(f"Thank you {name.title()} for taking the poll")
+
+# >Thank you Jen for taking the poll
+# >Thank you Phil for taking the poll
+# >Thank you Saraha for taking the poll
+
+print() #line gap
+# Looping Through All Values in a Dictionary
+# use the values() method to return a list of values without any keys
+for language in favourite_language.values():
+	print(language.title())
+
+# >Java
+# >Scala
+# >Swift
+
+print() #line gap
+# This approach pulls all the values from the dictionary without checking for repeats.
+# To see each without repetition, we can use a set()
+for language in set(favourite_language.values()):
+	print(language.title())
+
+# >Swift
+# >Java
+# >Scala
+
+print() #line gap
+# NOTE: You can build a set directly using braces and separating the elements with commas:
+languages = {'Java', 'Scala', 'Swift', 'Java'}
+print(languages)
+
+# >{'Java', 'Swift', 'Scala'}
+# It’s easy to mistake sets for dictionaries because they’re both wrapped in braces. 
+# When you see braces but no key-value pairs, you’re probably looking at a set. 
+# Unlike lists and dictionaries, sets do not retain items in any specific order.
+
+print() # line gap
+# NESTING
+
+# A List of Dictionaries
+alien_0 = {'color': 'green', 'points': 10}
+alien_1 = {'color': 'yellow', 'points': 15}
+alien_2 = {'color': 'red', 'points': 20}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+	print(alien)
+
+# >{'color': 'green', 'points': 10}
+# >{'color': 'yellow', 'points': 15}
+# >{'color': 'red', 'points': 20}
+
+print() #line gap
+# A more realistic example would involve more than three aliens 
+# with code that automatically generates each alien.
+
+# Empty list
+aliens = []
+
+# Make 30 aliens
+for i in range(0, 30):
+	slow_alien = {'color': 'green', 'points': 10, 'speed': 'slow'}
+	aliens.append(slow_alien)
+
+# Show first 5 aliens
+for alien in aliens[:5]:
+	print(f"Alien: {alien}")
+
+# Show total number of aliens
+print(f"Total number of aliens: {len(aliens)}")
+
+# >Alien: {'color': 'green', 'points': 10, 'speed': 'slow'}
+# >Alien: {'color': 'green', 'points': 10, 'speed': 'slow'}
+# >Alien: {'color': 'green', 'points': 10, 'speed': 'slow'}
+# >Alien: {'color': 'green', 'points': 10, 'speed': 'slow'}
+# >Alien: {'color': 'green', 'points': 10, 'speed': 'slow'}
+# >Total number of aliens: 30
+
+print() #line gap
+# to change the first three aliens to yellow, medium-speed aliens worth 10 points each
+for alien in aliens[:3]:
+	if alien['color'] == 'green':
+		alien['color'] = 'yellow'
+		alien['points'] = 15
+		alien['speed'] = 'medium'
+
+# Show first 5 aliens
+for alien in aliens[:5]:
+	print(f"Alien: {alien}")
+
+# Show total number of aliens
+print(f"Total number of aliens: {len(aliens)}")
+
+# >Alien: {'color': 'yellow', 'points': 15, 'speed': 'medium'}
+# >Alien: {'color': 'yellow', 'points': 15, 'speed': 'medium'}
+# >Alien: {'color': 'yellow', 'points': 15, 'speed': 'medium'}
+# >Alien: {'color': 'green', 'points': 10, 'speed': 'slow'}
+# >Alien: {'color': 'green', 'points': 10, 'speed': 'slow'}
+# >Total number of aliens: 30
+
+# A List in a Dictionary
